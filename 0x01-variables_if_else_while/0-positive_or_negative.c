1e593cfef7b9fd122379a955c2f8389590196a20
YettyMO <yetunde_omoniyi@yahoo.com>
2022-09-12 17:20:51 -0700

    positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -20,8 +20,7 @@
 int main(void)
 
 {
-
-	int n;
+        int n;
 
         srand(time(0));
 
@@ -29,15 +28,16 @@ int main(void)
 
 
         if (n > 0)
-         {
+         
                printf("%d is positive\n", n);
-         }
-	else if (n < 0)
-        {
-	       printf("%d is negative\n", n);
-        }
-	else
-	{      printf("%d is zero\n", n);
+         
+	else if (n == 0)
+        
+	       printf("%d is zero\n", n);
+       
+	else (n < 0)
+
+	     printf("%d is negative\n", n);
 
 	return (0);
 
